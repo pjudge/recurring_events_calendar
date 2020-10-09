2afac1e19b573dcbe7b53015e12b29ff3794cc10
Pauline Judge <paulinemjudge@gmail.com>
2020-10-09 14:55:18 -0400

    updates to webpack to compile react correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 const path = require('path');
-const isDevMode = process.env.NODE_ENV !== 'production';
 
 const config = {
   entry: {
     main: ["./js/src/index.jsx"]
   },
-  devtool: (isDevMode) ? 'source-map' : false,
-  mode: (isDevMode) ? 'development' : 'production',
   output: {
-    path: isDevMode ? path.resolve(__dirname, "js/dist_dev") : path.resolve(__dirname, "js/dist"),
+    path: path.resolve(__dirname, "js/dist"),
     filename: '[name].min.js'
   },
   resolve: {
